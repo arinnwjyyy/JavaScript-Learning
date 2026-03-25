2bcdffd014870a61b89727e7a72a13d6d78228d0
Arin ♡︎ <collabswitharin@gmail.com>
2026-03-25 10:50:26 +0700

    Add files via upload

---
+++ JavaScript-Sentence-Maker.js
@@ -0,0 +1,20 @@
+let adjective = "beautiful";
+let noun = "rabbit";
+let verb = "running";
+let place = "forest";
+let adjective2 = "cute";
+let noun2 = "carrot";
+
+var firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+//reassign
+adjective = "handsome";
+noun = "panda";
+verb = "eating";
+place = "bamboo forest";
+adjective2 = "big";
+noun2 = "bamboo";
+
+var secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
