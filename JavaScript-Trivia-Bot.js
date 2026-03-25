82e208ccab9cb5f36833071777d554898994305f
Arin ♡︎ <collabswitharin@gmail.com>
2026-03-25 10:58:38 +0700

    Add files via upload

---
+++ JavaScript-Trivia-Bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Jonoue";
+let botLocation = "Indonesia";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is a high-level programming.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is also more fun than other programming languages.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " helps a web developer to build its website.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
