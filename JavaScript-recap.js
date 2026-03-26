e58a777033af33205514854c23d7a508b32c85e9
Arin ♡︎ <collabswitharin@gmail.com>
2026-03-26 10:47:46 +0700

    Add files via upload
    This is what I learned and wanted to save here

---
+++ JavaScript-recap.js
@@ -0,0 +1,43 @@
+console.log("Hi there!");
+
+const botName = "Suakaa";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "Python";
+const topic = "print";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+
+console.log(subject.length);
+
+console.log(`Here is an example of using the length property on the word ${topic}.`);
+console.log(topic.length);
+
+console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+
+console.log(subject[0]);
+
+console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
+console.log(subject[1]);
+
+console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
+
+const lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is awesome.";
+
+console.log("Here are examples of finding the positions of substrings in the sentence.");
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("awesome"));
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
